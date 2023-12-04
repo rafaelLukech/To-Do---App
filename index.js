926edb34ba12e1d7cafa68b9dab679ba213e71e5
Rafael Luke <rafael.lukech@gmail.com>
2023-12-03 23:48:56 -0300

    salvando uam tarefa no banco de dados

--- index.js
+++ index.js
@@ -9,6 +9,31 @@ app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+//coverter dados do formulario em objeto javascript
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+//rotas
+app.post('/criar', (requisicao, respostas) => {
+    const descricao = requisicao.body.descricao
+    const completa = 0
+
+    const sql = `
+    INSERT INTO tarefas(descricao,completa)
+    VALUES ('${descricao}', '${completa}')
+    `
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        respostas.redirect('/')
+    })
+})
+
 app. get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
