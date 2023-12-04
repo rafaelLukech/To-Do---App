ff4c71c2a80a3e8e368e1814b99e078b430833cd
Rafael Luke <rafael.lukech@gmail.com>
2023-12-03 21:36:25 -0300

    deixando a mudança de tema dinâmica

---
+++ public/js/script.js
@@ -0,0 +1,59 @@
+function alterarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+    
+    if (tema) {
+        let novoTema
+       
+        if (tema === "light") {
+            novoTema = "dark"
+            button.innerHTML = ` <img src="/imagens/sun-icon.png"alt="icone de Sol">`
+            body.classList.remove("light")
+            body.classList.add("dark") 
+        } else {
+            novoTema = "light"
+            button.innerHTML = ` <img src="/imagens/moon-icon.png"alt="icone de Lua">`
+            body.classList.remove("dark")
+            body.classList.add("light")
+        }
+
+        localStorage.setItem("tema", novoTema)
+        return
+
+    }
+
+    localStorage.setItem("tema", "dark")
+    body.classList.add("dark")
+}
+
+function verificarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        if (tema === "dark") {
+            body.classList.add("dark")
+            button.innerHTML = ` <img src="/imagens/sun-icon.png"alt="icone de Sol">`
+        } else {
+            body.classList.add("light")
+            button.innerHTML = ` <img src="/imagens/moon-icon.png"alt="icone de Lua">`
+        }
+    
+    }
+
+}
+
+verificarTema()
+
+    
+        
+    
+
+
+    
+ 
+
+
+
