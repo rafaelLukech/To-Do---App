07a2dfca3e020831e1f70fcc0736da5395a41808
Rafael Luke <rafael.lukech@gmail.com>
2023-12-05 17:08:03 -0300

    exclusao de tarefas e finalizacao do projeto

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 //rotas
+
+app.get('/limpartarefas', (requisicao, resposta) => {
+    const sql = 'DELETE FROM tarefas'
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/excluir', (requisicao, resposta) => {
     const id = requisicao.body.id
 
