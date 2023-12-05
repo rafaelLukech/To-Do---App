ab32160896c42b0c93b679c862ef6cc43f89dd4e
Rafael Luke <rafael.lukech@gmail.com>
2023-12-05 17:01:53 -0300

    exclusao de tarefas individuais

--- index.js
+++ index.js
@@ -17,6 +17,23 @@ app.use(express.urlencoded({
 app.use(express.json())
 
 //rotas
+app.post('/excluir', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql =`
+    DELETE FROM tarefas
+    WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/completar', (requisicao, resposta) => {
     const id = requisicao.body.id
 
--- public/js/script.js
+++ public/js/script.js
@@ -22,6 +22,16 @@ function descompletarTarefa(id){
     window.location.reload()
 }
 
+function excluirTarefa(id){
+    fetch("http://localhost:3000/excluir", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ id })
+    })
+}
+
 function alterarTema() {
     const tema = localStorage.getItem("tema")
     const body = document.querySelector("body")
