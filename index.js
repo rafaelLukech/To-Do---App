8ea62270327813e914abcf390994179c7ac5b951
Rafael Luke <rafael.lukech@gmail.com>
2023-12-05 16:53:25 -0300

    filtro de tarefas ativas completo

--- index.js
+++ index.js
@@ -72,9 +72,29 @@ app.post('/criar', (requisicao, respostas) => {
 
 
 app.get('/ativas', (requisicao, resposta) => {
+    const sql = `
+        SELECT * FROM tarefas
+        WHERE completa = '0'
+    `
 
+    conexao.query(sql, (erro, dados) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        const tarefas = dados.map((dado) =>{
+            return {
+                id: dado.id,
+                descricao: dado.descricao,
+                completa: false
+            }
+        })
+
+        const quantidadeTarefas = tarefas.length
+
+        resposta.render('ativas', { tarefas, quantidadeTarefas })
+    })
 })
-
 
 app.get('/', (requisicao, resposta)=>{
     const sql = 'SELECT * FROM tarefas'
